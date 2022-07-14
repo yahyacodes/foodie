fa2d4c8f84b513ab11649723c01b533feea87f27
yahya.codin <yahya.codin@gmail.com>
2022-07-14 19:48:37 +0300

    Initial commit

--- script.js
+++ script.js
@@ -1,33 +1,18 @@
-const modal = document.querySelector('.modal')
-const  talk = document.querySelector('.talk')
-const close = document.querySelector('.close')
-const send = document.querySelector('.send')
-const contact = document.querySelector('#Contact')
-const touch = document.querySelector('.touch')
-const touch1 = document.querySelector('.touch1')
+const modal = document.querySelector(".modal");
+const talk = document.querySelector(".talk");
+const close = document.querySelector(".close");
+const send = document.querySelector(".send");
 
-modal.style.display = 'none'
+modal.style.display = "none";
 
-close.onclick = function() {
-    modal.style.display = 'none'
-}
+close.onclick = function () {
+  modal.style.display = "none";
+};
 
-send.onclick = function() {
-    modal.style.display = 'none'
-}
-
-contact.onclick = function() {
-    modal.style.display = 'block'
-}
-
-touch.onclick = function() {
-    modal.style.display = 'block'
-}
+send.onclick = function () {
+  modal.style.display = "none";
+};
 
 talk.onclick = function () {
-    modal.style.display = 'block'
-}
-
-touch1.onclick = function() {
-    modal.style.display = 'block'
-}
+  modal.style.display = "block";
+};
